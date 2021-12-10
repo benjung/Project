a83c26d591077f421fa31512f748664e4cca90cf
benjung <bjung1@babson.edu>
2021-12-09 22:30:29 -0500

    sitemap.js

---
+++ javascript/index.js
@@ -0,0 +1,37 @@
+let recipes = document.getElementById("recipe-img");
+
+recipes.addEventListener(
+  "mouseover",
+  function (event) {
+    event.target.style.width = "350px";
+    event.target.style.height = "350px";
+  },
+  false
+);
+
+recipes.addEventListener(
+  "mouseout",
+  function (event) {
+    event.target.style.width = "250px";
+    event.target.style.height = "200px";
+  },
+  false
+);
+
+let heading = document.getElementById("heading1");
+
+heading.addEventListener(
+  "mouseover",
+  function (event) {
+    event.target.style.fontSize = "30px";
+  },
+  false
+);
+
+heading.addEventListener(
+  "mouseout",
+  function (event) {
+    event.target.style.fontSize = "20px";
+  },
+  false
+);
---
+++ javascript/sitemap.js
@@ -0,0 +1,30 @@
+var recipe1 = document.getElementById("recipe1");
+console.log(recipe1);
+
+recipe1.addEventListener(
+  "mouseenter",
+  function (event) {
+    event.target.innerHTML = "Recipe #1: Kimchi Fried Rice with Pork Belly";
+  },
+  false
+);
+
+var recipe2 = document.getElementById("recipe2");
+
+recipe2.addEventListener(
+  "mouseenter",
+  function (event) {
+    event.target.innerHTML = "Recipe #2: Kimchi Soup with Pork Belly";
+  },
+  false
+);
+
+var recipe3 = document.getElementById("recipe3");
+
+recipe3.addEventListener(
+  "mouseenter",
+  function (event) {
+    event.target.innerHTML = "Recipe #3: Stir-fried Baby Octopus";
+  },
+  false
+);
