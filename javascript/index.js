7daeeb41630e0f68e147028cd9955b5abb02f113
benjung <bjung1@babson.edu>
2021-12-10 00:53:04 -0500

    table.css

---
+++ javascript/index.js
@@ -0,0 +1,35 @@
+let recipes = document.getElementById("recipe-img");
+
+recipes.addEventListener(
+  "mouseover",
+  function (event) {
+    event.target.style.width = "300px";
+    event.target.style.height = "250px";
+  },
+  false
+);
+
+recipes.addEventListener(
+  "mouseout",
+  function (event) {
+    event.target.style.width = "250px";
+    event.target.style.height = "200px";
+  },
+  false
+);
+
+heading.addEventListener(
+  "mouseover",
+  function (event) {
+    event.target.style.fontSize = "30px";
+  },
+  false
+);
+
+heading.addEventListener(
+  "mouseout",
+  function (event) {
+    event.target.style.fontSize = "20px";
+  },
+  false
+);
